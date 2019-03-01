383736369c6159f7bd287ad0180abb095b31fbfd
Niko Lazaris <nikolazaris@gmail.com>
2019-03-01 11:54:34 -0500

    Create tutorial page

--- app/controllers/tutorial.js
+++ app/controllers/tutorial.js
@@ -4,20 +4,22 @@ const GitHub = require("../services/github");
 const router = express.Router();
 
 router.get("/requesting", async function(req, res) {
-  const { search = "" } = req.query;
+  const { query = "" } = req.query;
 
+  console.log("SEARCH", query);
   if (!req.session.access_token) {
     return res.render("tutorial/index");
   }
 
   const github = new GitHub({ access_token: req.session.access_token });
   const results1 = await github.get("/user/starred");
-  const results2 = search.length > 0 ? await github.get("/search/repositories", { q: search }) : [];
+  let results2 = query.length > 0 ? await github.get("/search/repositories", { q: query }) : [];
+  results2 = results2.items || [];
 
   return res.render("tutorial/index", {
-    tutorial1: results1,
-    tutorial2: results2,
-    query: search
+    tutorial1: results1.slice(0, 5),
+    tutorial2: results2.slice(0, 5),
+    query: query
   });
 });
 
